a7c89f758ba4f34945dc8e8628c872f0d462b619
Melanie Tolvett S <melanietolvett@gmail.com>
2025-08-07 20:03:28 -0400

    feat: initialize frontend setup

---
+++ src/pages/HomePage.jsx
@@ -0,0 +1,7 @@
+export const HomePage = () => {
+    return (
+        <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100">
+            <h1>HomePage</h1>
+        </div>
+    )
+}
