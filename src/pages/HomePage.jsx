003f850bfb98d7e6270839955638a48895ca9fec
Melanie Tolvett S <melanietolvett@gmail.com>
2025-08-08 16:52:59 -0400

    feat: added form to upload document

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,7 +1,10 @@
+import { UploadDocumentForm } from '../components/UploadDocumentForm';
+
 export const HomePage = () => {
-    return (
-        <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100">
-            <h1>HomePage</h1>
-        </div>
-    )
-}
+	return (
+		<div className="flex flex-col items-center justify-center min-h-screen gap-y-10 bg-gray-100">
+			<h1 className="text-2xl font-semibold text-gray-700">Validador de Revisión técnica</h1>
+			<UploadDocumentForm />
+		</div>
+	);
+};
