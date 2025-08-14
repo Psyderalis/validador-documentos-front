310623098b359c1e2e244dfd60a1c217fb668379
Melanie Tolvett S <melanietolvett@gmail.com>
2025-08-14 17:58:46 -0400

    add qr scanner

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,9 +1,15 @@
-import { UploadDocumentForm } from '../components/UploadDocumentForm';
+
+// import { UploadDocumentForm } from '../components/UploadDocumentForm';´
+
+import { UploadDocumentForm } from "../components/QRReader";
+
 
 export const HomePage = () => {
 	return (
 		<div className="flex flex-col items-center justify-center min-h-screen gap-y-10 bg-gray-100">
-			<h1 className="text-2xl font-semibold text-gray-700">Validador de Revisión técnica</h1>
+			<h1 className="text-2xl font-semibold text-gray-700">
+				Validador de Revisión técnica
+			</h1>
 			<UploadDocumentForm />
 		</div>
 	);
